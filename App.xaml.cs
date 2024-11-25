08add2d7359f2efb4bc7d8dfa355b93394969297
Ana Buzea <anabuzea99@gmail.com>
2024-11-25 15:59:36 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BuzeaAnaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
