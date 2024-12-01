ce655ec84aa9a7233ccad30ff72ad0ee839b9467
Ana Buzea <anabuzea99@gmail.com>
2024-12-01 22:35:56 +0200

    lista cumparaturi

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace BuzeaAnaLab7
+﻿using System;
+using BuzeaAnaLab7.Data;
+using System.IO;
+
+namespace BuzeaAnaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(
+                        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
